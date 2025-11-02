eaa974d3b152b863d9edd25c31198560a8c4a7c1
pasha <pashabezk@mail.ru>
2025-11-03 02:20:52 +0300

    ci: setup gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/2670661-six-cities-6/',
   test: {
     globals: true,
     environment: 'jsdom',
